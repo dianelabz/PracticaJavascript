419f9acbd48c96953a1e56a4d21c4a267518aaad
dianelabz <dbarboza@4geeks.co>
2022-03-15 23:11:50 +0000

    formula de condicionales

---
+++ condicionales.js
@@ -0,0 +1,46 @@
+/*
+if(condicion){
+    //sentencias
+}
+
+if(condicion){
+    //sentencias
+} else {
+    //sentencias
+}
+
+if(condicion){
+    //sentencias
+} else if (condicion) {
+    //sentencias
+} else {
+    //sentencias
+}
+*/
+
+let a = 10;
+let b = 15;
+let c = 22;
+
+if (a<b) {
+    console.log ("a es menor que b")
+}
+if (a>b) {
+    console.log ("a es mayor que b")
+}
+if (a>b) {
+    console.log ("a es mayor que b")
+}
+else {
+    console.log ("a es menor que b")
+}
+
+if (a>b && a>c) {
+    console.log ("a es mayor que b y c")
+}
+else if (b>c) {
+    console.log ("b es mayor que a y c")
+}
+else {
+    console.log ("c es mayor que a y b")
+}
